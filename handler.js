9b37d3485ca56cd0dfc49a053c5505cbff8ce99e
Jamie Brown <jamiebrown201@hotmail.com>
2019-11-04 14:31:09 +0000

    add constituency check

--- handler.js
+++ handler.js
@@ -1,15 +1,38 @@
 "use strict";
+const fs = require("fs");
+const postcodes = JSON.parse(fs.readFileSync("./data/constituencyMatch.json"));
 
 module.exports.hello = async event => {
+  const constituency = getConstituency(event.pathParameters.postcode);
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event
-      },
-      null,
-      2
-    )
+    body: constituency
   };
 };
+
+function getConstituency(postcode) {
+  var postcodeArray = postcode.toUpperCase().trim();
+  if (!postcodeArray.includes(" ")) {
+    postcodeArray = postcodeArray.replace(/^(.*)(\d)/, "$1 $2");
+  }
+  postcodeArray = postcodeArray.split(" ");
+  var primary = postcodeArray[0];
+  var secondary = postcodeArray[1];
+  var final;
+
+  if (primary.length === 2) {
+    final = postcodes[primary[0]][primary[1]];
+  }
+  if (primary.length === 3) {
+    final = postcodes[primary[0]][primary[1]][primary[2]];
+  }
+  if (primary.length === 4) {
+    final = postcodes[primary[0]][primary[1]][primary[2]][primary[3]];
+  }
+  if (primary.length === 5) {
+    final =
+      postcodes[primary[0]][primary[1]][primary[2]][primary[3]][primary[5]];
+  }
+
+  return final.regEx.find(data => secondary.match(data.regEx)).constituency;
+}
